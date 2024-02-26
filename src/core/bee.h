a03e70525cd5af0c33eaedf35ec685b1cc932006
Anderson Freitas <freitas@disroot.org>
2024-02-26 22:49:36 +0200

    feat: added movement to the bees

--- src/core/bee.h
+++ src/core/bee.h
@@ -6,6 +6,11 @@ private:
     bool isMad = false;
     AnimationController *flying;
     olc::vf2d *position;
+    olc::vf2d *travelTo;
+    olc::vf2d *initialPosition;
+    float speed = 100.0f;
+    float delta = 0.0f;
+    int direction = 1;
 
 public:
     void OnCreate() override
@@ -24,12 +29,40 @@ public:
             this->isMad = isMadField.value();
         }
 
+        const auto &travelToField = beeEntity.getField<ldtk::FieldType::Point>("travel");
+
+        if (!travelToField.is_null())
+        {
+            ldtk::IntPoint travelTo = travelToField.value();
+            this->travelTo = new olc::vf2d({static_cast<float>(travelTo.x), static_cast<float>(travelTo.y)});
+        }
+
         position = new olc::vf2d({static_cast<float>(initialPosition.x), static_cast<float>(initialPosition.y)});
+        this->initialPosition = new olc::vf2d(*position);
     }
 
     void OnProcess(float fElapsedTime) override
     {
         Camera *camera = GetLayer()->GetCamera();
+
+        if (travelTo != nullptr)
+        {
+            delta += fElapsedTime;
+            position->x += speed * fElapsedTime * direction;
+
+            if (position->x > initialPosition->x + travelTo->x * 32)
+            {
+                delta = 0.0f;
+                direction = -1;
+            }
+            else if (position->x < initialPosition->x)
+            {
+                delta = 0.0f;
+                direction = 1;
+            }
+            position->y = initialPosition->y + travelTo->y * 32 + sin(delta * 2) * 10;
+        }
+
         if (!camera->IsOnScreen(*position))
         {
             return;
@@ -44,7 +77,12 @@ public:
             frame.x += 32 * 2;
         }
 
-        map->DrawTile(pos, flying->tilesetId, frame, {32.0f, 32.0f}, {1.0f, 1.0f});
+        if (direction == -1)
+        {
+            pos.x += 32;
+        }
+
+        map->DrawTile(pos, flying->tilesetId, frame, {32.0f, 32.0f}, {1.0f * direction, 1.0f});
 
         // Draw collider
         if (DEBUG)
